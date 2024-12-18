b868fe92d10971def06853d0f381ab561d010e9a
Andrea Spoldi <andrea.spoldi@docebo.com>
2024-12-18 10:05:44 +0100

    feat: changing ports

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ async fn main() {
     env_logger::init();
     info!("Starting Rusteze...");
 
-    let listen_addr = "127.0.0.1:6379".to_string();
-    let backend_addr = "127.0.0.1:6380".to_string(); // Example backend Redis address
+    let listen_addr = "127.0.0.1:6380".to_string();
+    let backend_addr = "127.0.0.1:6379".to_string(); // Example backend Redis address
 
     let manager = ConnectionManager::new(listen_addr, backend_addr);
     if let Err(e) = manager.run().await {
