6402c1b9564e40c628727117ec39ed58a42b8018
Andrea Spoldi <andrea.spoldi@docebo.com>
2024-12-19 09:19:13 +0100

    feat: trying to implement multiplexing

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,66 +1,134 @@
-use log::info;
+use crate::connection::ConnectionPool;
+use log::{error, info};
+use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
+use tokio::sync::{mpsc, Mutex};
 
 #[derive(Clone)]
 pub struct Proxy {
-    backend_addr: String,
+    connection_pool: Arc<ConnectionPool>,
 }
 
 impl Proxy {
-    pub fn new(backend_addr: String) -> Self {
-        Proxy { backend_addr }
+    pub fn new(connection_pool: Arc<ConnectionPool>) -> Self {
+        Proxy { connection_pool }
     }
 
     pub async fn handle_connection(
         &self,
         client_socket: TcpStream,
     ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        // Connect to the backend Redis server
-        let backend_socket = TcpStream::connect(&self.backend_addr).await?;
-        info!("Connected to backend Redis at {}", self.backend_addr);
+        // Get the backend connection from the pool
+        let backend_connection = self.connection_pool.get_connection().await?;
 
-        // Split both sockets into read and write halves
-        let (client_reader, client_writer) = tokio::io::split(client_socket);
-        let (backend_reader, backend_writer) = tokio::io::split(backend_socket);
+        // Split the client socket into reader and writer halves
+        let (mut client_reader, mut client_writer) = tokio::io::split(client_socket);
 
-        // Spawn tasks for bi-directional data transfer
-        let client_to_backend = tokio::spawn(Self::transfer_data(
-            client_reader,
-            backend_writer,
-            "Client -> Backend",
-        ));
+        // Create channels for bi-directional communication
+        let (client_to_backend_tx, mut client_to_backend_rx) =
+            mpsc::channel::<Result<Vec<u8>, std::io::Error>>(1024);
+        let (backend_to_client_tx, mut backend_to_client_rx) =
+            mpsc::channel::<Result<Vec<u8>, std::io::Error>>(1024);
 
-        let backend_to_client = tokio::spawn(Self::transfer_data(
-            backend_reader,
-            client_writer,
-            "Backend -> Client",
-        ));
+        // Task: Forward data from client to backend
+        let client_to_backend_task = tokio::spawn({
+            let backend_writer = backend_connection.clone();
+            async move {
+                let mut backend_writer = backend_writer.lock().await;
+                loop {
+                    match client_to_backend_rx.recv().await {
+                        Some(Ok(buf)) => {
+                            if let Err(e) = backend_writer.write_all(&buf).await {
+                                error!("Error writing to backend: {}", e);
+                                break;
+                            }
+                        }
+                        Some(Err(e)) => {
+                            error!("Error reading from client: {}", e);
+                            break;
+                        }
+                        None => break, // Sender closed
+                    }
+                }
+                backend_writer.shutdown().await.ok();
+            }
+        });
 
-        // Wait for both tasks to complete
-        let _ = tokio::try_join!(client_to_backend, backend_to_client)?;
+        // Task: Read data from backend and send to the client via channel
+        let backend_to_client_task = tokio::spawn({
+            let backend_reader = backend_connection.clone();
+            async move {
+                let mut backend_reader = backend_reader.lock().await;
+                let mut buf = vec![0u8; 1024];
+                loop {
+                    match backend_reader.read(&mut buf).await {
+                        Ok(0) => break, // EOF
+                        Ok(n) => {
+                            if let Err(e) = backend_to_client_tx.send(Ok(buf[..n].to_vec())).await {
+                                error!("Error sending to client: {}", e);
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            error!("Error reading from backend: {}", e);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        // Task: Read data from client and send to the backend via channel
+        let client_to_backend_send = tokio::spawn({
+            let mut buf = vec![0u8; 1024];
+            async move {
+                loop {
+                    match client_reader.read(&mut buf).await {
+                        Ok(0) => break, // EOF
+                        Ok(n) => {
+                            if let Err(e) = client_to_backend_tx.send(Ok(buf[..n].to_vec())).await {
+                                error!("Error sending to backend: {}", e);
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            error!("Error reading from client: {}", e);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        // Task: Forward data from backend to client
+        let backend_to_client_receive = tokio::spawn(async move {
+            loop {
+                match backend_to_client_rx.recv().await {
+                    Some(Ok(buf)) => {
+                        if let Err(e) = client_writer.write_all(&buf).await {
+                            error!("Error writing to client: {}", e);
+                            break;
+                        }
+                    }
+                    Some(Err(e)) => {
+                        error!("Error reading from backend: {}", e);
+                        break;
+                    }
+                    None => break, // Sender closed
+                }
+            }
+            client_writer.shutdown().await.ok();
+        });
+
+        // Wait for all tasks to complete
+        tokio::try_join!(
+            client_to_backend_task,
+            backend_to_client_task,
+            client_to_backend_send,
+            backend_to_client_receive
+        )?;
 
         Ok(())
-    }
-
-    async fn transfer_data<R, W>(
-        mut reader: R,
-        mut writer: W,
-        direction: &str,
-    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
-    where
-        R: AsyncReadExt + Unpin,
-        W: AsyncWriteExt + Unpin,
-    {
-        let mut buffer = vec![0; 1024];
-        loop {
-            let n = reader.read(&mut buffer).await?;
-            if n == 0 {
-                info!("{}: Connection closed", direction);
-                return Ok(());
-            }
-            writer.write_all(&buffer[..n]).await?;
-            writer.flush().await?;
-        }
     }
 }
